46d2f7138072b1f4343a9d93be5602c5ee050c1b
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2022-01-07 03:12:30 +0300

    initial commit

---
+++ RSSchool_T12/RSSchool_T12UITests/RSSchool_T12UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RSSchool_T12UITestsLaunchTests.swift
+//  RSSchool_T12UITests
+//
+//  Created by Evgeniy Petlitskiy on 7.01.22.
+//
+
+import XCTest
+
+class RSSchool_T12UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
