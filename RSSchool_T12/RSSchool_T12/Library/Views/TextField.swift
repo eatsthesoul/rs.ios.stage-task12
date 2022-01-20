9dbb4f0ae4c00e3c53e91bd2797117b071ac7441
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2022-01-20 19:34:17 +0300

    feat: add custom TextField view

---
+++ RSSchool_T12/RSSchool_T12/Library/Views/TextField.swift
@@ -0,0 +1,47 @@
+//
+//  TextField.swift
+//  RSSchool_T12
+//
+//  Created by Evgeniy Petlitskiy on 20.01.22.
+//
+
+import UIKit
+
+class TextField: UITextField {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get { layer.cornerRadius }
+        set { layer.cornerRadius = newValue }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupAppearance()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupAppearance()
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        bounds.insetBy(dx: 20, dy: 20)
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        bounds.insetBy(dx: 20, dy: 20)
+    }
+    
+    private func setupAppearance() {
+        backgroundColor = .clear
+        font = UIFont.montserrat(24, .semibold)
+        textColor = .rickBlack
+        
+        borderStyle = .none
+        adjustsFontSizeToFitWidth = false
+        
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.rickBlack.cgColor
+    }
+
+}
